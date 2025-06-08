cb1c13fc729a1a622fde264b2856ade06a1e8f25
wilson <Wilson.sz@flexsystem.com>
2025-06-08 10:06:03 +0800

    + start_dockge.bat, start_dockge.sh

---
+++ docker/start_dockge.bat
@@ -0,0 +1,13 @@
+@echo off
+
+REM 下载Dockge镜像
+ docker pull louislam/dockge:1
+
+REM 运行Dockge容器
+ docker run -p 5001:5001 --name dockge ^
+ -e "DOCKGE_STACKS_DIR=/opt/stacks" ^
+ -e "DOCKGE_ENABLE_CONSOLE=true" ^
+ -v /var/run/docker.sock:/var/run/docker.sock ^
+ -v /mydata/dockge/data:/app/data ^
+ -v /mydata/dockge/stacks:/opt/stacks ^
+ -d louislam/dockge:1
